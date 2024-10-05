d3db7d12008cc8d45dbfba80f29b674df80a68a5
Kti2309 <Cotinegreyra104@gmail.com>
2024-10-04 21:29:08 -0300

    commit

--- js/product-info.js
+++ js/product-info.js
@@ -1,17 +1,11 @@
 
-let product ;
-
 const params = new URLSearchParams(window.location.search);
 const productId = parseInt(params.get("id"));
-const catID = localStorage.getItem("catID");
-
-let catName;
-console.log(catName);
+ 
 
 console.log("El ID del producto seleccionado es: ", productId);
-console.log(catID);
 
-const url = "https://japceibal.github.io/emercado-api/cats_products/"+catID+".json";
+const url = "https://japceibal.github.io/emercado-api/products/"+productId+".json";
 const productDetailsContainer = document.getElementById("containerProduct");
 
 function showProductDetails(product) {
@@ -21,13 +15,36 @@ function showProductDetails(product) {
     } else {
     productDetailsContainer.innerHTML =`
         <div class="product-details">
-        <img class="imgcar" src="${product.image}" alt="${product.name}">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img class="imgcar" src="${product.images[0]}" alt="${product.name}">
+        </div>
+        <div class="carousel-item">
+         <img class="imgcar" src="${product.images[1]}" alt="${product.name}">
+        </div>
+         <div class="carousel-item">
+         <img class="imgcar" src="${product.images[2]}" alt="${product.name}">
+        </div>
+        <div class="carousel-item">
+        <img class="imgcar" src="${product.images[3]}" alt="${product.name}">
+        </div>
+         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Next</span>
+        </button>
+        </div>
         <div class="detailsProduct"  >
         <h2 class="product-Title" >${product.name}</h2>
-        <h3 class="product-category">${catName}</h2>
+        <h3 class="product-category">${product.category}</h2>
         <p class="price"> ${product.currency} ${product.cost}</p>
         <p><span class="title-description">Descripción:</span> ${product.description}</p>
-        <p class="id">Id: ${productId}</p>
+        <p class="id">Id: ${product.id}</p>
         <p class="sold">Cantidad de vendidos: ${product.soldCount}</p>
         </div>
         `;
@@ -39,38 +56,33 @@ fetch(url)
         throw new Error("No se pueden mostrar los datos");
     }
     return response.json();
+    
 })
 
 .then(data => {
-    const products = data.products;
-    catName = data.catName;
-    
-    const product = products.find(p => p.id === productId);
+    productInfo = data;
 
-    if (product) {
-        showProductDetails(product);
-    } else {
-        alert("Producto no encontrado.");
-        window.location.href = "categories.html";
-    }
+    console.log(data);
+    showProductDetails(data);
 })
     
-///
+// Hay que mostrar los coments baby
 
+urlComments = "https://japceibal.github.io/emercado-api/products_comments/"+productId+".json"
 let comments;
 
 
-const commentId = parseInt(params.get("product"));
-const commentID = localStorage.getItem("commentId");
+/*const commentId = parseInt(params.get("product"));
+const commentID = localStorage.getItem("commentId");*/
 
 let commentName;
-console.log(commentName);
+console.log(urlComments);
 
 console.log("El ID del producto seleccionado es: ", commentID);
 console.log(commentID);
 
 
-const productCommentsContainer = document.getElementById("containerProduct");
+const productCommentsContainer = document.getElementById("containerComment");
 
 function showProductDetailsComments(product) {
 
@@ -79,6 +91,31 @@ function showProductDetailsComments(product) {
     } else {
         productCommentsContainer.innerHTML +=`
         <div class="product-comment">
+        <div class="containerForm" >
+      <form  method="post">
+        <label><alert>¡Deja tu comentario!</alert></label>
+         <div class="elem-group"> <label for="name">Nombre de Usuario</label> 
+          <input type="text" id="name" name="visitor_name" placeholder="Nombre de ejemplo" required>
+         </div> 
+         <div class="dateName"><label for="date" > Fecha</label>
+          <input type="date" id="start" name="date" placeholder="00/00/0000" />
+         </div>
+            <div class="elem-group"> 
+              <label for="message">Comentario</label>
+               <textarea id="message" name="visitor_message" placeholder="Escribe tu mensaje aquí." required></textarea>
+               </div> 
+               <div>
+                <btn class="fa fa-star"></btn>
+                <btn class="fa fa-star"></btn>
+                <btn class="fa fa-star"></btn>
+                <btn class="fa fa-star"></btn>
+                <btn class="fa fa-star"></btn>
+               </div>
+               <button type="submit">Enviar Comentario</button>
+              </form>
+             
+
+    </div>
             <p><strong>Usuario:</strong> ${product.user}</p>
             <p><strong>Fecha:</strong> ${product.dateTime}</p>
             <p><strong>Comentario:</strong> ${product.description}</p>
@@ -96,8 +133,8 @@ fetch(url)
 })
 
 .then(data => {
-    comments = data;
-    const productComments = comments.find(c => c.comment === commentId);
+    comment = data;
+    const productComments = product.find(c => c.comment === commentId);
     
    
 
@@ -107,5 +144,4 @@ fetch(url)
         alert("Producto no encontrado.");
         window.location.href = "categories.html";
     }
-})
-    
+});
